625c65dc47a36600951195a45b826baf89a23ee1
Artem Musalitin <musalitin.a.a@gmail.com>
2024-04-19 20:43:13 +0200

    Add move model

---
+++ app/models/move.rb
@@ -0,0 +1,14 @@
+class Move < ApplicationRecord
+  X_SYMBOL = :x
+  O_SYMBOL = :o
+
+  belongs_to :game
+
+  validates :row, :column, :symbol, presence: true
+
+  validates :row, :column, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 3 }
+  validates :row, uniqueness: { scope: [:game_id, :column] }
+  validates :column, uniqueness: { scope: [:game_id, :row] }
+
+  validates :symbol, inclusion: { in: [X_SYMBOL, O_SYMBOL] }
+end

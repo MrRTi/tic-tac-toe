2a0c648fc250b49f070879ef39388a80b3ad71de
Artem Musalitin <musalitin.a.a@gmail.com>
2024-04-20 14:50:20 +0200

    Add state machine into game class

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,19 @@
 class Game < ApplicationRecord
+  include AASM
+
   has_many :moves, dependent: :destroy
+
+  aasm column: :state do
+    state :in_progress, initial: true
+    state :finished
+    state :draw
+
+    event :finish do
+      transitions from: :in_progress, to: :finished
+    end
+
+    event :draw do
+      transitions from: :in_progress, to: :draw
+    end
+  end
 end

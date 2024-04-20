c0a350a34a3a1fc804290355d5da583275b03ba3
Artem Musalitin <musalitin.a.a@gmail.com>
2024-04-20 19:47:52 +0200

    Fix games#show request specs

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,28 +1,29 @@
 RSpec.describe "Games" do
-  describe "GET /index" do
+  describe "GET /games" do
     it "returns http success" do
       get "/games"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
+  describe "GET /games/:game_id" do
     context "when game is in database" do
       let!(:game) { create(:game) }
 
       it "returns http success" do
-        get "/games", params: { game_id: game.id }
+        get "/games/#{game.id}"
         expect(response).to have_http_status(:success)
       end
     end
 
     it "returns http not found" do
-      get "/game/"
+      get "/games/0"
+
       expect(response).to have_http_status(:not_found)
     end
   end
 
-  describe "POST /create" do
+  describe "POST /games" do
     it "returns http success" do
       post "/games"
       expect(response).to redirect_to(game_path(Game.last))

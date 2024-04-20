d779938fb3237eab7726c773a7521bd19f3da51d
Artem Musalitin <musalitin.a.a@gmail.com>
2024-04-20 20:06:35 +0200

    Refactor games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,8 @@
 class GamesController < ApplicationController
+  GAMES_LIST_LIMIT = 5
+
   def index
-    @games = Game.order(created_at: :desc).limit(5)
+    @games = Game.order(created_at: :desc).limit(GAMES_LIST_LIMIT)
   end
 
   def show
@@ -9,12 +11,18 @@ class GamesController < ApplicationController
     @board = BoardService.new(moves, max_rows: @game.max_rows, max_columns: @game.max_columns).call
 
     last_move_symbol = moves.last&.symbol
-    @next_symbol = last_move_symbol == Move::O_SYMBOL ? Move::X_SYMBOL : Move::O_SYMBOL
+    @next_symbol = next_symbol(last_move_symbol)
     @winner_symbol = @game.finished? && last_move_symbol
   end
 
   def create
     game = Game.create
     redirect_to game_path(game)
+  end
+
+  private
+
+  def next_symbol(last_move_symbol)
+    last_move_symbol == Move::O_SYMBOL ? Move::X_SYMBOL : Move::O_SYMBOL
   end
 end

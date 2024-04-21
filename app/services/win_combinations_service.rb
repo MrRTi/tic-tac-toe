4d322b4223e8ecef2b46a70501a456088a77db88
Artem Musalitin <musalitin.a.a@gmail.com>
2024-04-21 14:02:55 +0200

    Add cache to win combinations calculation

--- app/services/win_combinations_service.rb
+++ app/services/win_combinations_service.rb
@@ -5,14 +5,9 @@ class WinCombinationsService
   end
 
   def call
-    rows_combinations = (0...max_rows * max_columns).each_slice(max_columns).to_a
-    columns_combinations = rows_combinations.transpose
-
-    combinations = rows_combinations + columns_combinations
-    combinations << diagonal(rows_combinations)
-    combinations << diagonal(rows_combinations.reverse)
-
-    combinations
+    Rails.cache.fetch("win_comb-#{max_rows}x#{max_columns}", expires_in: 24.hours) do
+      combinations
+    end
   end
 
   private
@@ -21,5 +16,16 @@ class WinCombinationsService
 
   def diagonal(array)
     (0...max_rows).map { |i| array[i][i] }.sort
+  end
+
+  def combinations
+    rows_combinations = (0...max_rows * max_columns).each_slice(max_columns).to_a
+    columns_combinations = rows_combinations.transpose
+
+    combinations = rows_combinations + columns_combinations
+    combinations << diagonal(rows_combinations)
+    combinations << diagonal(rows_combinations.reverse)
+
+    combinations
   end
 end
